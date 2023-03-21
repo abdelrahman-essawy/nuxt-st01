7f77ed2c6f84323146e8162c09c7c78681e0b357
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-03-21 03:15:40 +0200

    clean code for navbar

--- my-app/src/components/navbar/Navbar.tsx
+++ my-app/src/components/navbar/Navbar.tsx
@@ -1,51 +1,50 @@
-import Button from '@/app/utilities/Button'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import styles from './navbar.module.css'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Button from '@/app/utilities/Button';
+import styles from './navbar.module.css';
+
+interface User {
+    avatar: string;
+    name: string;
+}
 
 interface NavbarProps {
-    signIn: () => void
-    signOut: () => void
-    data: any
-    status: 'loading' | 'authenticated' | 'unauthenticated'
+    signIn: () => void;
+    signOut: () => void;
+    data: { user: User } | null;
+    status: 'loading' | 'authenticated' | 'unauthenticated';
 }
 
 export const Navbar = ({ signIn, signOut, data, status }: NavbarProps) => {
+    const renderLoading = () => <div>Loading...</div>;
+
+    const renderAuthenticated = () => (
+        <div className={styles.user}>
+            <Image src={data?.user?.avatar || ''} alt="user image" width={40} height={40} priority />
+            {data?.user.name}
+            <Button onClick={signOut}>Sign Out</Button>
+        </div>
+    );
+
+    const renderUnauthenticated = () => <Button onClick={signIn}>Sign In</Button>;
+
     return (
         <>
-            <div
-                className={styles.center}>
+            <div className={styles.center}>
                 <Link href="/">
-                    <Image
-                        className={styles.logo}
-                        src="/next.svg"
-                        alt="Next.js Logo"
-                        width={180}
-                        height={37}
-                        priority
-                    />
+                    <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
                 </Link>
                 <div className={styles.thirteen}>
                     <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
                 </div>
             </div>
 
-            {
-                status === 'loading' ?
-                    <div>Loading...</div>
-                    :
-                    status === 'authenticated' &&
-                        data ?
-                        <div className={styles.user}>
-                            <Image src={data.user.avatar} alt="user image" width={40} height={40} priority />
-                            {data.user.name}
-                            <Button onClick={signOut}>Sign Out</Button>
-                        </div>
-                        :
-                        <Button onClick={signIn}>Sign In</Button>
-
-            }
+            {status === 'loading'
+                ? renderLoading()
+                : status === 'authenticated'
+                    ? renderAuthenticated()
+                    : renderUnauthenticated()}
         </>
-    )
-}
+    );
+};
