0ea5d0492c95c213c6c9bb13d37e6ebfa08830dd
Abdelrahman Essawy <boda.m7amd@gmail.com>
2023-03-21 03:20:58 +0200

    clean code for [...nextauth]

--- my-app/src/pages/api/auth/[...nextauth].ts
+++ my-app/src/pages/api/auth/[...nextauth].ts
@@ -1,28 +1,25 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-
+// Helper function to get the list of users from the server
 async function getUsers() {
-    // return await fsPromises.readFile(path.join(process.cwd(), 'data', '../../../../../server/data/users.json'), 'utf-8')
-    //     .then((data) => {
-    //         return JSON.parse(data);
-    //     })
-
     try {
-        return await fetch('http://localhost:3000/users.json')
-            .then((res) => res.json())
-    }
-    catch (err) {
-        console.log(err)
-        return []
+        const response = await fetch('http://localhost:3000/users.json');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+        return [];
     }
 }
 
-const authenticate = async (username: string, password: string) => {
+// Authenticates the user based on the given username and password
+async function authenticate(username: string, password: string) {
     const users = await getUsers();
     return users.find((user: any) => user.username === username && user.password === password);
 }
 
+// Configuration options for NextAuth
 const authOptions: NextAuthOptions = {
     session: {
         strategy: "jwt",
@@ -31,34 +28,33 @@ const authOptions: NextAuthOptions = {
         CredentialsProvider({
             type: "credentials",
             credentials: {},
-            authorize(credentials) {
-                const { username, password } = credentials as {
-                    username: string;
-                    password: string;
-                };
-
-                return authenticate(username, password) ?? new Error('Wrong credentials. Try again.')
-
+            async authorize(credentials) {
+                const { username, password } = credentials as { username: string, password: string };
+                const user = await authenticate(username, password);
+                if (!user) {
+                    throw new Error('Wrong credentials. Try again.');
+                }
+                return user;
             },
         }),
     ],
     pages: {
         signIn: "/signin",
-        // error: '/auth/error',
-        // signOut: '/auth/signout'
+        // error: '/error',
+        // signOut: '/signout'
     },
     callbacks: {
         jwt: async ({ token, user }) => {
-            user && (token.user = user)
-            return token
+            if (user) {
+                token.user = user;
+            }
+            return token;
         },
         session: async ({ session, token }) => {
-            session.user = token.user || {}
-            return session
+            session.user = token.user || {};
+            return session;
         },
     },
 };
 
 export default NextAuth(authOptions);
-
-
